2f1218e28ec12c37588330f4ca7d0d1f6e2d848a
Josueminchala <Josueminchala44@gmail.com>
2024-09-04 23:34:15 -0400

    Added about me page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Intro } from './components/HeadingPage/intro';
+import { AboutMe } from './components/AboutMe/test';
 function App() {
   return (
     <div className="App">
       <Intro name='Josue'/>
+      <AboutMe aboutMe=''/>
     </div>
   );
 }
---
+++ src/components/AboutMe/test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+type AboutMeProps = {
+    aboutMe: string;
+  };
+
+  export const AboutMe = ({ aboutMe }: AboutMeProps) => (
+    <div>
+        <h1>
+            About Me
+        </h1>
+    </div>
+
+  )
