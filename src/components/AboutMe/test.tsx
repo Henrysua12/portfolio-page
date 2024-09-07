14a680cf36d6576a49257761ef578115c841144c
Josueminchala <Josueminchala44@gmail.com>
2024-09-06 21:17:55 -0400

    Configured main page and added tailwind for styling

--- src/components/AboutMe/test.tsx
+++ src/components/AboutMe/test.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import './index.css';
+
 type AboutMeProps = {
     aboutMe: string;
   };
 
   export const AboutMe = ({ aboutMe }: AboutMeProps) => (
-    <div>
-        <h1>
-            About Me
-        </h1>
+    <div className="bg-white-smoke p-4 h-screen">
+        <h1 className="text-3xl font-bold text-blue-900 underline">About Me</h1>  
+        <p className="text-secondary">
+            I am a Motivated computer science student with a recent career change driven by a deep passion for technology, seeking to
+            enhance skills and contribute to the development of innovative solutions that leverage cutting-edge technologies to
+            address real-world challenges.<br></br>
+            In my spare time, I love going to the gym, hanging out with my friends and playing basketball from time to time.
+            I also love traveling to new places and eating all kinds of different foods.
+          </p>
     </div>
 
-  )
+  );
--- src/components/HeadingPage/intro.tsx
+++ src/components/HeadingPage/intro.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
 type IntroProps = {
   name: string;
 };
 
+// Style MUI Button with Tailwind utility classes
+const StyledButton = styled(Button)(({ theme }) => ({
+  borderRadius: '0.375rem', // Tailwind's rounded-md
+  padding: '0.5rem 1rem', // Tailwind's py-2 px-4
+  textTransform: 'none', // Disable uppercase transformation
+}));
+
 export const Intro = ({ name }: IntroProps) => (
-  <div>
-    <div>
-      <h2> Hi my name is {name}</h2>
-      <h2> I'm a Full Stack Software Engineer </h2>
+  <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
+    <div className="text-center mb-8">
+      <h2 className="text-4xl font-bold mb-4">Hi, my name is {name}</h2>
+      <h2 className="text-2xl font-semibold">I'm a Full Stack Software Engineer</h2>
     </div>
-    <div>
-      <Button>Resume Cv</Button>
-      <Button>Contact Me</Button>
+    <div className="flex space-x-4">
+      <StyledButton variant="contained" color="primary" className="bg-blue-500 text-white hover:bg-blue-600">
+        Resume Cv
+      </StyledButton>
+      <StyledButton variant="contained" color="secondary" className="bg-green-500 text-white hover:bg-green-600">
+        Contact Me
+      </StyledButton>
     </div>
   </div>
 );
+
+// flex and flex-col are containers that are common patterns and 
